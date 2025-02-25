49243fa2b9eb22049307f9b896872fb760117aa5
Mohit Gupta <110718346+mgupta70@users.noreply.github.com>
2025-02-24 23:04:53 -0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -31,6 +31,10 @@ VIDEOS_DICT = {
     'video_1': VIDEO_DIR / 'video_1.mp4',
     'video_2': VIDEO_DIR / 'video_2.mp4',
     'video_3': VIDEO_DIR / 'video_3.mp4',
+    'ped_1' : VIDEO_DIR / 'pedestrian_crossing.mp4',
+    'ped_2' : VIDEO_DIR / 'pedestrian_crossing2.mp4',
+    'car_1' : VIDEO_DIR / 'cars.mp4',
+    'car_2' : VIDEO_DIR / 'cars_trucks.mp4', 
 }
 
 # ML Model config
